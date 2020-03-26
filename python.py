3aaefc0e33778183e767fae24cc26df834031533
shivamgupta.ram <shivamgupta.ram@gmail.com>
2020-03-26 21:19:28 +0530

    third

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("Index.html")
 @app.route('/about')
 def about():
     return render_template("about.html")
